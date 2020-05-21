1da47c41f57d8d910a99fe1c5f83fa47e83eaa3d
Ilkka Miettinen <ivmiettinen@gmail.com>
2020-05-21 15:03:56 +0300

    fine tuning

--- src/components/CountryFilterItem.js
+++ src/components/CountryFilterItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CountryFilterItem = ({ weather }) => {
   //   console.log('itemi:', weather.current);
 
-  console.log('weather:', weather);
+  // console.log('weather:', weather);
 
   return (
     <div>
