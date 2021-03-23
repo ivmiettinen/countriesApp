48a5271fc676306c47a2bec53a80d1f4cd4ca324
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2021-03-23 15:11:22 +0200

    added different more weather forecasts and weather forecast images

--- src/components/CountryFilterItem.js
+++ src/components/CountryFilterItem.js
@@ -27,22 +27,71 @@ const CountryFilterItem = ({ weather, forecast }) => {
             {weather.map((weather) => {
                 return (
                     <div key={weather.city.id}>
-                        <b>Temperature:</b>
-                        <p>
-                            {temperatureConverter(
-                                weather.list[0].main.temp
-                            ).toFixed(2)}{' '}
-                            celsius
-                        </p>
-                        <p>{weather.list[0].weather[0].icon} </p>
-                        <br />
-                        <img
-                            src={
-                                images[
-                                    `${weather.list[0].weather[0].icon}@2x.png`
-                                ]
-                            }
-                        />
+                        <div>
+                            <b>Temperature:</b>
+                            <p>Time: {weather.list[0].dt_txt}</p>
+                            <p>
+                                {temperatureConverter(
+                                    weather.list[0].main.temp
+                                ).toFixed(2)}{' '}
+                                celsius
+                            </p>
+                            <p>{weather.list[0].weather[0].icon} </p>
+                            <br />
+                            <img
+                                src={
+                                    images[
+                                        `${weather.list[0].weather[0].icon}@2x.png`
+                                    ]
+                                }
+                            />
+                        </div>
+                        <div>
+                            <p>Time: {weather.list[1].dt_txt}</p>
+                            <p>
+                                {temperatureConverter(
+                                    weather.list[1].main.temp
+                                ).toFixed(2)}{' '}
+                                celsius
+                            </p>
+                            <img
+                                src={
+                                    images[
+                                        `${weather.list[1].weather[0].icon}@2x.png`
+                                    ]
+                                    
+                                }
+
+                            />
+                        </div>
+                        <p>Time: {weather.list[2].dt_txt}</p>
+                            <p>
+                                {temperatureConverter(
+                                    weather.list[2].main.temp
+                                ).toFixed(2)}{' '}
+                                celsius
+                            </p>
+                            <img
+                                src={
+                                    images[
+                                        `${weather.list[2].weather[0].icon}@2x.png`
+                                    ]
+                                }
+                            />
+                            <p>Time: {weather.list[3].dt_txt}</p>
+                            <p>
+                                {temperatureConverter(
+                                    weather.list[3].main.temp
+                                ).toFixed(2)}{' '}
+                                celsius
+                            </p>
+                            <img
+                                src={
+                                    images[
+                                        `${weather.list[3].weather[0].icon}@2x.png`
+                                    ]
+                                }
+                            />
                     </div>
                 )
             })}
